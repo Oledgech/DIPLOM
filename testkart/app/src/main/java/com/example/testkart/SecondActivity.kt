471a0e3715b269bd7635e52725d499c725777586
Oledgech <you@example.com>
2025-04-10 16:21:17 +0300

    1 неделя

---
+++ testkart/app/src/main/java/com/example/testkart/SecondActivity.kt
@@ -0,0 +1,34 @@
+package com.example.testkart
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+    public fun startNewActivity(v: View)
+    {
+        val intent = Intent(this, MainActivity::class.java)
+        val nameText = findViewById<EditText>(R.id.editTextNumber)
+        intent.putExtra("username", nameText.text.toString())
+        startActivity(intent)
+    }
+
+}
